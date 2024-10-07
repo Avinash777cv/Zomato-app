00a5d0ef6cc5c5325ee3d95a7e31f563d377d74d
Avinash77cv <avinashcv00@gmail.com>
2024-10-07 15:04:24 +0530

    created zomato app

---
+++ Zomato.java
@@ -0,0 +1,14 @@
+class Zomato{
+
+public void findAllFood(){
+
+System.out.println("displayed all food");
+
+}
+
+public void addFoodToCart(int id){
+
+System.out.println("add successfully");
+
+}
+}
